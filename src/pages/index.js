87708e8b6740d8d4420bc43dbb2a48a35191d8bf
Ursa Kacar <kacar.ursa@gmail.com>
2020-04-03 23:21:14 +0200

    adds first page images and thumbs and all info and todo list and text changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,39 +5,58 @@ import Layout from '../components/layout'
 // import Lightbox from 'react-images'
 import Gallery from '../components/Gallery'
 
-import thumb01 from '../assets/images/thumbs/01.jpg'
-import thumb02 from '../assets/images/thumbs/02.jpg'
-import thumb03 from '../assets/images/thumbs/03.jpg'
-import thumb04 from '../assets/images/thumbs/04.jpg'
-import thumb05 from '../assets/images/thumbs/05.jpg'
-import thumb06 from '../assets/images/thumbs/06.jpg'
-import thumb07 from '../assets/images/thumbs/07.jpg'
-import thumb08 from '../assets/images/thumbs/08.jpg'
-import thumb09 from '../assets/images/thumbs/09.jpg'
-import thumb10 from '../assets/images/thumbs/10.jpg'
+import thumb01 from '../assets/images/first-page/thumb-bela-vrtnica.jpg'
+import thumb02 from '../assets/images/first-page/thumb-zitno-polje.png'
+import thumb03 from '../assets/images/first-page/thumb-vinograd.png'
+import thumb04 from '../assets/images/first-page/thumb-drvarji.jpg'
+import thumb05 from '../assets/images/first-page/thumb-galeb.png'
+import thumb06 from '../assets/images/first-page/thumb-gnezdo.jpg'
+import thumb07 from '../assets/images/first-page/thumb-kruh.png'
+import thumb08 from '../assets/images/first-page/thumb-moc.png'
+import thumb09 from '../assets/images/first-page/thumb-ob-sori.jpg'
+import thumb10 from '../assets/images/first-page/thumb-jesenski-macesni.png'
+import thumb11 from '../assets/images/first-page/thumb-strazar-zitnih-polj.png'
+import thumb12 from '../assets/images/first-page/thumb-snezi.png'
+import thumb13 from '../assets/images/first-page/thumb-soncnice.png'
+import thumb14 from '../assets/images/first-page/thumb-sopek.png'
 
-import full01 from '../assets/images/fulls/01.jpg'
-import full02 from '../assets/images/fulls/02.jpg'
-import full03 from '../assets/images/fulls/03.jpg'
-import full04 from '../assets/images/fulls/04.jpg'
-import full05 from '../assets/images/fulls/05.jpg'
-import full06 from '../assets/images/fulls/06.jpg'
-import full07 from '../assets/images/fulls/07.jpg'
-import full08 from '../assets/images/fulls/08.jpg'
-import full09 from '../assets/images/fulls/09.jpg'
-import full10 from '../assets/images/fulls/10.jpg'
+import full01 from '../assets/images/first-page/bela-vrtnica.jpg'
+import full02 from '../assets/images/first-page/zitno-polje.png'
+import full03 from '../assets/images/first-page/vinograd.png'
+import full04 from '../assets/images/first-page/drvarji.jpg'
+import full05 from '../assets/images/first-page/galeb.png'
+import full06 from '../assets/images/first-page/gnezdo.jpg'
+import full07 from '../assets/images/first-page/kruh.png'
+import full08 from '../assets/images/first-page/moc.png'
+import full09 from '../assets/images/first-page/ob-sori.jpg'
+import full10 from '../assets/images/first-page/jesenski-macesni.png'
+import full11 from '../assets/images/first-page/strazar-zitnih-polj.png'
+import full12 from '../assets/images/first-page/snezi.png'
+import full13 from '../assets/images/first-page/soncnice.png'
+import full14 from '../assets/images/first-page/sopek.png'
 
 const DEFAULT_IMAGES = [
-    { id: '1', source: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '2', source: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '3', source: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '4', source: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '5', source: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '6', source: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '7', source: full07, thumbnail: thumb07, caption: 'Photo 7', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '8', source: full08, thumbnail: thumb08, caption: 'Photo 8', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '9', source: full09, thumbnail: thumb09, caption: 'Photo 9', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '10', source: full10, thumbnail: thumb10, caption: 'Photo 10', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
+    { id: '14', source: full14, thumbnail: thumb14, caption: 'Rdeče vrnice', description: '45x60 cm, olje na platno, prodano'},
+    { id: '9', source: full09, thumbnail: thumb09, caption: 'Ob jezeru', description: '55x80 cm, olje na platno, prodano'},
+
+    { id: '2', source: full02, thumbnail: thumb02, caption: 'Žitno polje', description: '120x90 cm, olje na platno, prodano'},
+    { id: '5', source: full05, thumbnail: thumb05, caption: 'Galeb', description: '120x100 cm, olje na platno, prodano'},
+
+    { id: '10', source: full10, thumbnail: thumb10, caption: 'Jesen', description: '100x80 cm, olje na platno, prodanoo'},
+    { id: '12', source: full12, thumbnail: thumb12, caption: 'Sneži', description: '55x80 cm, olje na platno, prodano'},
+
+    { id: '13', source: full13, thumbnail: thumb13, caption: 'Sončnice v modri vazi', description: '50x40 cm, olje na platno, prodano'},
+    { id: '8', source: full08, thumbnail: thumb08, caption: 'Moč', description: '100x100 cm, olje na platno, prodano'},
+
+    { id: '7', source: full07, thumbnail: thumb07, caption: 'Kruh', description: '100x100 cm, olje na platno, prodano'},
+    { id: '6', source: full06, thumbnail: thumb06, caption: 'Gnezdo', description: '100x100 cm, olje na platno, prodano'},
+
+    { id: '4', source: full04, thumbnail: thumb04, caption: 'Drvarji', description: '95x100 cm, olje na platno, prodano'},
+    { id: '1', source: full01, thumbnail: thumb01, caption: 'Bela vrtnica', description: '90x70 cm, olje na platno, prodano'},
+
+    { id: '11', source: full11, thumbnail: thumb11, caption: 'Stražar žitnih polj', description: '100x100 cm, olje na platno, prodano'},
+    { id: '3', source: full03, thumbnail: thumb03, caption: 'Vinograd', description: '65x50 cm, olje na platno, prodano'},
+
 ];
 
 class HomeIndex extends React.Component {
@@ -59,9 +78,17 @@ class HomeIndex extends React.Component {
                         <header className="major">
                             <h2>O meni/kratka predstavitev/lahko tudi kaksen drug naslov.</h2>
                         </header>
-                        <p>Text... Rabim text, kratko biografijo, info, moznost slike po narocilu
+                        <p>Text... Rabim text, kratko biografijo, info, pac predstavitveno besedilo
 
                         Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
+
+                        <h2>TODO (URSA) </h2>
+                        <ul className="major">
+                            <li>styling: text color (everything is kinda bland right now), avatar size, link colors</li>
+                            <li>implement gallery/full portfolio</li>
+                            <li>implement biography page</li>
+                            <li>figure out how current gallery is implemeted and fix the stupid shit</li>
+                        </ul>
                         <ul className="actions">
                             <li><a href="/biography" className="button custom-hidden">Biografija</a></li>
                         </ul>
@@ -84,7 +111,7 @@ class HomeIndex extends React.Component {
 
                     <section id="three">
                         <h2>Kontakt</h2>
-                        <p>Blabla, lahko se dogovorimo za ogled galerije, delavnice, slikarsko solo, itd itd.</p>
+                        <p>Tukaj rabim text glede ponudbe: "Lahko se dogovorimo za ogled galerije, delavnice, slikarsko solo, slike po narocilu, in tako dalje....</p>
                         <div className="row">
                             <div className="8u 12u$(small)">
                                 <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field" action="/contact-success" >
@@ -109,7 +136,7 @@ class HomeIndex extends React.Component {
                                     </li>
                                     <li>
                                         <h3 className="icon fa-mobile"><span className="label">Telefon</span></h3>
-                                        +386 31 654 197
+                                        +386 31 433 275
                                     </li>
                                     <li>
                                         <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
